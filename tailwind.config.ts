73dbb0401844c1b3757b85eb15b3d7efaf0367f1
mishoshup <202131799+mishoshup@users.noreply.github.com>
2025-04-19 15:41:32 +0800

    Add prettier format for tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss';
+
+export default <Partial<Config>>{
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+  ],
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+};
